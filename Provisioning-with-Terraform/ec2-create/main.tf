5681a55f457ba23d5ddd38cda884b43414ba6625
rkdaas <gupta.ritik2002@gmail.com>
2025-04-25 11:53:53 +0000

    EC2-create demo

---
+++ Provisioning-with-Terraform/ec2-create/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "demo" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "Terraform-EC2-Demo"
+  }
+}
